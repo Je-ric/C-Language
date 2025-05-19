76fc8ff011b81ec093a0eae9a0dd04fb0046241f
Je-ric <delacruz.jeric.j@gmail.com>
2025-05-19 17:49:37 +0800

    Add new pattern programs to Clean directory

---
+++ Pattern/Clean/4.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+		
+		int i,j;
+		for(i=1;i<=5;i++)
+		{
+			printf("\n");
+			for(j=1;j<=5;j++)
+					if(i==3 || i==5 || i==1 || j==5)
+							printf("%2d",3);
+					else
+							printf("%2c",' ');
+					
+		}
+		return 0;
+}
